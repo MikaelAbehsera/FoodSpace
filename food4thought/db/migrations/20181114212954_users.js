b0f5f912bfe376b6db4e9d27a687d567649d8d12
char55 <haras.charlotte@gmail.com>
2018-11-15 14:18:44 +0000

    starting migrations

---
+++ food4thought/db/migrations/20181114212954_users.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable("users", function (table) {
+      table.increments();
+      table.string("username");
+      table.string("email");
+      table.string("password");
+      table.string("profileIMG");
+      table.string("location");
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable("users"),
+  ]);
+};
---
+++ food4thought/db/migrations/20181114213353_faves.js
@@ -0,0 +1,28 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable("faves", function (table) {
+      table.increments();
+      table.integer("recipe_id");
+      table.integer("user_id");
+
+      table
+      .foreign("recipe_id")
+      .references("id")
+      .on("recipes")
+      .onDelete("cascade");
+
+    table
+      .foreign("user_id")
+      .references("id")
+      .on("users")
+      .onDelete("cascade");
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable("faves"),
+  ]);
+};
