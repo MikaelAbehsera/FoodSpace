4e4909e0d4a6f750f7c68cd326ab485efd0e8721
Mikael Abehsera <Mikael.abehsera@gmail.com>
2018-11-15 10:13:01 -0500

    boilerplate setup with react navigator

--- food4thought/App.js
+++ food4thought/App.js
@@ -1,19 +1,56 @@
 import React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, Image, Platform, View, Text } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
+class LogoTitle extends React.Component {
+  render() {
+    return (
+      <Image
+        source={require('./media/cc.png')}
+        style={{ width: 30, height: 30 }}
+      />
+    );
+  }
+}
+
 class HomeScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerTitle: <LogoTitle />,
+      headerRight: (
+        <Button
+          onPress={navigation.getParam('increaseCount')}
+          title="+1"
+          color={Platform.OS === 'ios' ? '#fff' : null}
+        />
+      ),
+    };
+  };
+
+  componentWillMount() {
+    this.props.navigation.setParams({ increaseCount: this._increaseCount });
+  }
+
+  state = {
+    count: 0,
+  };
+
+  _increaseCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
+        <Text>Count: {this.state.count}</Text>
         <Button
           title="Go to Details"
           onPress={() => {
             /* 1. Navigate to the Details route with params */
             this.props.navigation.navigate('Details', {
               itemId: 86,
-              otherParam: 'anything you want here',
+              otherParam: 'First Details',
             });
           }}
         />
@@ -23,11 +60,24 @@ class HomeScreen extends React.Component {
 }
 
 class DetailsScreen extends React.Component {
+  static navigationOptions = ({ navigation, navigationOptions }) => {
+    const { params } = navigation.state;
+
+    return {
+      title: params ? params.otherParam : 'A Nested Details Screen',
+      /* These values are used instead of the shared configuration! */
+      headerStyle: {
+        backgroundColor: navigationOptions.headerTintColor,
+      },
+      headerTintColor: navigationOptions.headerStyle.backgroundColor,
+    };
+  };
+
   render() {
-    /* 2. Get the param, provide a fallback value if not available */
-    const { navigation } = this.props;
-    const itemId = navigation.getParam('itemId', 'NO-ID');
-    const otherParam = navigation.getParam('otherParam', 'some default value');
+    /* 2. Read the params from the navigation state */
+    const { params } = this.props.navigation.state;
+    const itemId = params ? params.itemId : null;
+    const otherParam = params ? params.otherParam : null;
 
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -35,15 +85,13 @@ class DetailsScreen extends React.Component {
         <Text>itemId: {JSON.stringify(itemId)}</Text>
         <Text>otherParam: {JSON.stringify(otherParam)}</Text>
         <Button
-          title="Go to Details... again"
+          title="Update the title"
           onPress={() =>
-            this.props.navigation.push('Details', {
-              itemId: Math.floor(Math.random() * 100),
-            })}
+            this.props.navigation.setParams({ otherParam: 'Updated!' })}
         />
         <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
+          title="Go to Details... again"
+          onPress={() => this.props.navigation.navigate('Details')}
         />
         <Button
           title="Go back"
@@ -56,14 +104,26 @@ class DetailsScreen extends React.Component {
 
 const RootStack = createStackNavigator(
   {
-    Home: HomeScreen,
-    Details: DetailsScreen,
+    Home: {
+      screen: HomeScreen,
+    },
+    Details: {
+      screen: DetailsScreen,
+    },
   },
   {
     initialRouteName: 'Home',
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
   }
 );
-
 
 export default class App extends React.Component {
   render() {
