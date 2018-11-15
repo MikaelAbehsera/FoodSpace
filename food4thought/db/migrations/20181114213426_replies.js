cd4f97fe5fc0c48ea0f363071638c5376180b38b
char55 <haras.charlotte@gmail.com>
2018-11-15 15:12:53 +0000

    user&connected  migrations made

--- food4thought/db/migrations/20181114213426_replies.js
+++ food4thought/db/migrations/20181114213426_replies.js
@@ -1,8 +1,36 @@
 
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable("replies", function (table) {
+      table.increments();
+      table.integer("recipe_id");
+      table.integer("suggestion_id");
+      table.integer("user_id");
+      table.string("reply");
+
+      table
+      .foreign("recipe_id")
+      .references("id")
+      .on("recipes")
+      .onDelete("cascade");
+
+      table
+      .foreign("suggestion_id")
+      .references("id")
+      .on("suggestions")
+      .onDelete("cascade");
+
+    table
+      .foreign("user_id")
+      .references("id")
+      .on("users")
+      .onDelete("cascade");
+    })
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable("replies"),
+  ]);
 };
--- food4thought/db/migrations/20181114213438_mademeals.js
+++ food4thought/db/migrations/20181114213438_mademeals.js
@@ -1,8 +1,28 @@
 
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable("mademeals", function (table) {
+      table.increments();
+      table.integer("recipe_id");
+      table.integer("user_id");
+
+      table
+      .foreign("recipe_id")
+      .references("id")
+      .on("recipes")
+      .onDelete("cascade");
+
+    table
+      .foreign("user_id")
+      .references("id")
+      .on("users")
+      .onDelete("cascade");
+    })
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable("mademeals"),
+  ]);
 };
