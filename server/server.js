bf04703341dc86ec84f40c1da2f971d7e4680b74
char55 <haras.charlotte@gmail.com>
2018-11-18 13:46:51 +0000

    post script made for recipe create form

--- server/server.js
+++ server/server.js
@@ -82,10 +82,64 @@ app.post("/login" , (req, res) => {
 
 app.post("/create" , (req, res) => {
   // creates new recipe
+
+  const recipeForm = req.body.form;
+  const ingredientsArray = req.body.ingredients;
+  const instructionsArray = req.body.instructions;
+  
+  // STILL NEED TO TAG CATEGORIES AND PROPER USER_ID
+  // currently being simulated
+
   knex("recipies")
-  .insert({})
-  .returning()
+  .insert({
+    name: recipeForm.recipeName,
+    description: recipeForm.recipeDescription,
+    overall_rating: null,
+    time: recipeForm.timeToMake,
+    difficulty: recipeForm.difficultyOfRecipe,
+    creator_id: 1
+  })
+  .returning("id")
   .then((id) => {
+    const ingredientsList = []
+    const instructionsList = []
+
+    ingredientsArray.forEach((single) => {
+      ingredientsList.push({ 
+          recipes_id: id[0],
+          food_type: single.foodType,
+          quantity: single.quantity
+        })
+    })
+    instructionsArray.forEach((single) => {
+      instructionsList.push({ 
+          recipes_id: id[0],
+          step_number: single.stepNumber,
+          step_description: single.step
+        })
+    })
+    const addTag = {
+      recipes_id: id[0],
+      category_id: 3      
+    }
+    knex("ingredients")
+    .insert(ingredientsList)
+    .then(()=> {
+      knex("instructions")
+      .insert(instructionsList)
+      .then(() => {
+        knex("tags")
+        .insert(addTag)
+      })
+      .catch((err) => {
+        res.json({id: -1, success: false})
+        res.status(404)
+        console.log(err); throw err;
+      })
+      .finally(() => {
+        res.json({recipes_id: id[0], success: true})
+      });
+    })
     
 
   })
@@ -175,3 +229,14 @@ app.post("/mealmade" , (req, res) => {
     })
 
 });
+
+
+
+app.post("/review" , (req, res) => {
+  // add review to a recipe
+
+  
+  });
+
+// can just have a function get called whenever a new review is made, uses knex to look over all previous reviews for that recipe, calculates the average and updates the recipe "overall_rating"
+// USE KNEX avg function
