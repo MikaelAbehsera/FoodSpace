9a8e186d8d2f96564fc65d11df37c94d8cb76113
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-11-27 18:18:43 -0500

    clean up cat seed file

--- server/db/seeds/04categories.js
+++ server/db/seeds/04categories.js
@@ -10,12 +10,10 @@ exports.seed = function(knex, Promise) {
             category_name: "Greasy",
             category_description: "This chategory is reserved for those who look to gorge on the greasiet meals without shame."
           }),
-
           knex("categories").insert({
             category_name: "Health nut",
             category_description: "This chategory is low in calories and high in vegitables. It is reserved for those who desire that banging beach bod."
           }),
-
           knex("categories").insert({
             category_name: "Munchies",
             category_description: "This category is reserved for those who get home at ungodly hour looking find sustenance in a pinch"
