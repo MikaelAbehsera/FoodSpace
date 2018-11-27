30dee9352ebebde77d94545f935aa1efbbf58d30
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-11-27 18:18:17 -0500

    clean up recipe seed file

--- server/db/seeds/05ingredients.js
+++ server/db/seeds/05ingredients.js
@@ -12,43 +12,36 @@ exports.seed = function(knex, Promise) {
             food_type: "hot sauce", 
             quantity: "3 tsp"
           }),
-
           knex("ingredients").insert({
             recipes_id: 1,
             food_type: "bagel", 
             quantity: "1"
           }),
-
           knex("ingredients").insert({
             recipes_id: 1,
             food_type: "peanut butter", 
             quantity: "4 tbsp"
           }),
-
           knex("ingredients").insert({
             recipes_id: 2,
             food_type: "mince meat", 
             quantity: "1 lbs"
           }),
-          
           knex("ingredients").insert({
             recipes_id: 2,
             food_type: "bread", 
             quantity: "1"
           }),
-          
           knex("ingredients").insert({
             recipes_id: 2,
             food_type: "carrots", 
             quantity: "16 grams"
           }),
-          
           knex("ingredients").insert({
             recipes_id: 2,
             food_type: "milk", 
             quantity: "20 ml"
           }),
-          
           knex("ingredients").insert({
             recipes_id: 2,
             food_type: "egg", 
@@ -60,93 +53,76 @@ exports.seed = function(knex, Promise) {
             food_type: "peanut butter", 
             quantity: "3 tbsp"
           }),
-          
           knex("ingredients").insert({
             recipes_id: 3,
             food_type: "bread", 
             quantity: "2 slices"
           }),
-          
           knex("ingredients").insert({
             recipes_id: 3,
             food_type: "butter", 
             quantity: "2 tbsp"
           }),
-          
           knex("ingredients").insert({
             recipes_id: 3,
             food_type: "jam", 
             quantity: "3 tbsp"
           }),
-
-
           knex("ingredients").insert({
             recipes_id: 4,
             food_type: "leftover rice", 
             quantity: "1 cup"
           }),
-
           knex("ingredients").insert({
             recipes_id: 4,
             food_type: "egg", 
             quantity: "2"
           }),
-
           knex("ingredients").insert({
             recipes_id: 4,
             food_type: "salt and peper", 
             quantity: "1"
           }),
-
           knex("ingredients").insert({
             recipes_id: 4,
             food_type: "frozen peas/carrots", 
             quantity: "1/4 cup"
           }),
-
-
-           knex("ingredients").insert({
+          knex("ingredients").insert({
             recipes_id: 5,
             food_type: "steak", 
             quantity: "2 140z tbone"
           }),
-
           knex("ingredients").insert({
             recipes_id: 5,
             food_type: "salt and peper", 
             quantity: "1"
           }),
-
           knex("ingredients").insert({
             recipes_id: 5,
             food_type: "potatoes", 
             quantity: "4"
           }),
-
           knex("ingredients").insert({
             recipes_id: 6,
             food_type: "pasta", 
             quantity: "2 servings"
           }),
-          
           knex("ingredients").insert({
             recipes_id: 6,
             food_type: "tomato sauce", 
             quantity: "4 ounces"
           }),
-          
           knex("ingredients").insert({
             recipes_id: 6,
             food_type: "Mince meat", 
             quantity: "1 lb"
           }),
-          
           knex("ingredients").insert({
             recipes_id: 7,
             food_type: "butter", 
             quantity: "1 cup"
           }),
-          
           knex("ingredients").insert({
             recipes_id: 7,
             food_type: "egg", 
@@ -157,19 +133,16 @@ exports.seed = function(knex, Promise) {
             food_type: "bread", 
             quantity: "180 grams"
           }),
-          
           knex("ingredients").insert({
             recipes_id: 8,
             food_type: "white or brown rice", 
             quantity: "2 cups"
           }),
-          
           knex("ingredients").insert({
             recipes_id: 8,
             food_type: "butter", 
             quantity: "2 tbsp"
           }),
-          
           knex("ingredients").insert({
             recipes_id: 8,
             food_type: "Green olives", 
